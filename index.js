f008bd18cdacad73878db32176a31a8975c9282d
laithchikhaoui17 <laithchikhaoui17@gmail.com>
2023-12-16 14:41:51 +0100

    only:1,5,6

--- index.js
+++ index.js
@@ -29,3 +29,32 @@ function iterateAndLogWithWhile(n){
 
     return n+'isodd'
 }
+/*****************************  5  ***********************/
+Using a for loop THEN a while loop
+ Create a function that calculates the sum of a number as follows:
+ factorial(3); => 3+2+1 = 6
+ factorial(5); => 5+4+3+2+1 = 15
+
+ function sumWithWhile(number){
+    var x = 0
+    for ( var i=1 ; i<=number ;i++){
+        x=x+i
+    }
+    return x
+
+}
+/*****************************  6  ***********************/
+
+ Using recursion
+ Create a function that calculates the factorial of a number
+ factorial(3); => 3*2*1 = 6
+ factorial(5); => 5*4*3*2*1 = 120
+
+  function factorialRecursively(number){
+    var x = 1
+    for ( var i=1 ; i<=number ;i++){
+        x=x*i
+    }
+    return x
+
+}
